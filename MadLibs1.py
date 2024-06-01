a4be0019d7703a93d3f32cda96f5d175a7d7d176
Eric Bast <eric.r.bast@gmail.com>
2024-05-31 21:28:47 -0500

    update to game

--- MadLibs1.py
+++ MadLibs1.py
@@ -2,8 +2,24 @@ print("Lets play Mad Libs.")
 print("Are you ready?")
 
 pNoun1 = input("Please give me a proper name. ")
-print("What gender is" + pNoun1, "?" "boy, girl, or non?")
+print("What gender is " + pNoun1 +"?" " A boy, girl, or non?")
+
 assign = input()
+instance = False
+while not instance:
+    if assign == "boy":
+        gen = ["he", "his"]
+        instance = True
+    elif assign == "girl":
+        gen = ["she","her"]
+        instance = True
+    elif assign == "non":
+        gen = ["they", "their"]
+        instance = True
+    else:
+        print("please use a valid entry")
+        assign = input()
+
 adj1 = input("Please give me an adjective. ")
 noun2 = input("Please give me a noun. ")
 verb1 = input("Please give me a verb. ")
@@ -11,17 +27,8 @@ bpart1 = input("Please give me a plural body part. ")
 celeb1 = input("Please give me a Celebrity Name. ")
 num1 = input("Please give me a number. ")
 animal1 = input("Please give an animal(plural). ")
-
-if assign == "boy":
-    gen = ["he", "his"]
-elif assign == "girl":
-    gen = ["she","her"]
-elif assign == "non":
-    gen = ["they", "theirs"]
-else:
-    print("please use a valid entry")
-
-print("Today I will tell you a story about", pNoun1, "and", gen[1], "adventure in the land of",adj1 +"ville.") 
+   
+print("Today I will tell you a story about ", pNoun1, "and", gen[1], "adventure in the land of",adj1 +"ville.") 
 print("One day", pNoun1, "was sitting on", gen[1], noun2, "when a huge wind stated to blow")
 print(pNoun1, "was quickly able to", verb1, "away from", gen[1], noun2, "but the wind was too strong and", gen[0], "landed on", gen[1], bpart1)
 print("Fortunately ,", celeb1, "was right there to catch", pNoun1, "and prevented any serious damage from happening")
