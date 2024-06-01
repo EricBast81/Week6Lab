143e8a9a41aa979a900e03f08364b482c5f3449d
Eric Bast <eric.r.bast@gmail.com>
2024-05-31 20:50:11 -0500

    updating game

--- MadLibs1.py
+++ MadLibs1.py
@@ -15,7 +15,7 @@ animal1 = input("Please give an animal(plural). ")
 if assign == "boy":
     gen = ["he", "his"]
 elif assign == "girl":
-    gen = ["her","hers"]
+    gen = ["she","her"]
 elif assign == "non":
     gen = ["they", "theirs"]
 else:
@@ -23,7 +23,7 @@ else:
 
 print("Today I will tell you a story about", pNoun1, "and", gen[0], "adventure in the land of",adj1 +"ville.") 
 print("One day", pNoun1, "was sitting on", gen[0], noun2, "when a huge wind stated to blow")
-print(pNoun1, "was quickly able to", verb1, "away from", gen[0], noun2, "but the wind was too strong and", gen[0], "landed on", gen[1], bpart1)
+print(pNoun1, "was quickly able to", verb1, "away from", gen[1], noun2, "but the wind was too strong and", gen[0], "landed on", gen[1], bpart1)
 print("Fortunately ,", celeb1, "was right there to catch", pNoun1, "and prevented any serious damage from happening")
 print(pNoun1, "was so thankfull they decided to give", celeb1, "a tip of $" + num1, "dollars.")
 print(celeb1, "was so overjoyed they asked you to be in their next movie with them")
